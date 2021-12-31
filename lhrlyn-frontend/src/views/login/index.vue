8f9e4f4280f020113baf6e8e4be673401c65653d
lihaoran <1258774709@qq.com>
2021-12-31 13:07:35 +0800

    feat:dev分支前端代码的创建

---
+++ lhrlyn-frontend/src/views/login/index.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="page">
+    <img class="img" src="../../static/1198282.jpg">
+    <div class="login">
+      <div>
+        <span>userName:{{ form.userName }}-</span>
+        <span>password:{{ form.password }}</span>
+      </div>
+      <el-form ref="form" class="loginForm" :model="form" :inline="false" size="normal">
+        <h1 class="title">芝麻开门</h1>
+        <el-form-item>
+          <el-input v-model="form.username" placeholder="账号" class="username" />
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="form.password" placeholder="密码" class="password" />
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="form.code" placeholder="请输入正确答案" />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="begin()">begin</el-button>
+          <el-button>centrl</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { login } from '../../api/user'
+export default {
+  data() {
+    return {
+      login,
+      form: {
+        username: undefined,
+        password: undefined,
+        code: undefined
+      },
+      user: {
+        username: undefined,
+        password: undefined
+      }
+    }
+  },
+  methods: {
+    async begin() {
+      console.log(this.form.username)
+      console.log(this.form.password)
+      this.user.username = this.form.username
+      this.user.password = this.form.password
+      const response = await login(this.user)
+      console.log(response.data)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.page {
+
+}
+.img {
+  width: 1700px;
+  position: absolute;
+  z-index: -1;
+}
+.login{
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+}
+.loginForm{
+  height: 350px;
+  width: 400px;
+  box-shadow: 0 0 25px #cacac6;
+}
+.username{
+
+}
+.password{
+}
+.title{
+  text-align: center;
+  color: wheat
+}
+</style>
---
+++ lhrlyn-frontend/src/views/test/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app-container">
+    <el-button>aaaa</el-button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Test',
+  data() {
+    return {
+    }
+  },
+  created() {
+    this.getList()
+  },
+  methods: {
+  }
+}
+</script>
+
